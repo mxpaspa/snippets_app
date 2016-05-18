379f52f6dd2b6699462c79ef73be45758da2333c
mxpaspa <mxpaspa@gmail.com>
2016-05-18 00:28:31 +0000

    putting the pieces together completed

--- snippets.py
+++ snippets.py
@@ -1,18 +1,21 @@
 import logging
 import argparse
-
+import psycopg2
 
 # Set the log output file, and the log level
 logging.basicConfig(filename="snippets.log", level=logging.DEBUG)
-
+logging.debug("Connecting to PostgreSQL")
+connection = psycopg2.connect(database="snippets")
+logging.debug("Database connection established.")
 
 def put(name, snippet):
-    """
-    Store a snippet with an associated name.
-
-    Returns the name and the snippet
-    """
-    logging.error("FIXME: Unimplemented - put({!r}, {!r})".format(name, snippet))
+    """Store a snippet with an associated name."""
+    logging.info("Storing snippet {!r}: {!r}".format(name, snippet))
+    cursor = connection.cursor()
+    command = "insert into snippets values (%s, %s)"
+    cursor.execute(command, (name, snippet))
+    connection.commit()
+    logging.debug("Snippet stored successfully.")
     return name, snippet
     
     
@@ -32,22 +35,19 @@ def main():
     """Main function"""
     logging.info("Constructing parser")
     parser = argparse.ArgumentParser(description="Store and retrieve snippets of text")
-    arguments = parser.parse_args()
-
-
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
 
     # Subparser for the put command
-    logging.debug("Constructing put subparser")
+    logging.info("Constructing put subparser")
     put_parser = subparsers.add_parser("put", help="Store a snippet")
     put_parser.add_argument("name", help="Name of the snippet")
     put_parser.add_argument("snippet", help="Snippet text")
     
     get_parser = subparsers.add_parser("get", help="Retrieve a snippet")
     get_parser.add_argument("name", help="Name of the snippet")
-    arguments = parser.parse_args()
     
     arguments = parser.parse_args()
+    print (arguments)
     # Convert parsed arguments from Namespace to dictionary
     arguments = vars(arguments)
     command = arguments.pop("command")
@@ -60,7 +60,7 @@ def main():
         print("Retrieved snippet: {!r}".format(snippet))
     
     
+
+
 if __name__ == "__main__":
     main()
-    
-put(**arguments)
